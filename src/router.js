5fc2033ea5814ff460cc711a015dd53994ba9540
Abhishek Sharma <abhivsharma8@gmail.com>
2021-08-22 13:46:02 +0530

    Redirect and Aliases

--- src/router.js
+++ src/router.js
@@ -1,30 +1,63 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from "vue-router";
 
-import Dashboard from './components/Dashboard.vue';
-import Tasks from './components/todos/TodoItems.vue';
-import NotesAddEdit from './components/notes/NoteAddEdit.vue';
-import NotesView from './views/NotesView.vue'
-import NotFound from './components/NotFound.vue';
+import Dashboard from "./components/Dashboard.vue";
+import Tasks from "./components/todos/TodoItems.vue";
+import NotesAddEdit from "./components/notes/NoteAddEdit.vue";
+import NotesView from "./views/NotesView.vue";
+import NotFound from "./components/NotFound.vue";
+
+import { store } from "./store";
+import NavbarComponent from "./components/navigation/Navbar.vue";
 
 const router = createRouter({
-  history: createWebHashHistory(),  
-  routes : [
-    { path: '/', component: Dashboard },
-    { path: '/dashboard', component: Dashboard },
-    { path: '/tasks', component: Tasks },
-    { path: '/notes', component: NotesView,
-      children: [
-        { path: 'new', component: NotesAddEdit }, 
-        { path: 'edit/:noteId([0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12})', 
-          component: NotesAddEdit,
-          props: route => ({ id: route.params.noteId })
-        }
-      ]
+  history: createWebHashHistory(),
+  routes: [
+    {
+      path: "/",
+      redirect: (to) => {
+        console.log("Redirect from ", to);
+        return { path: store.getters.startScreen };
+      },
     },
-    { path: '/:pathMatch(.*)', component: NotFound }
+    {
+      path: "/dashboard",
+      name: "dashboard",
+      alias: "/home",
+      components: {
+        default: Dashboard,
+        Navbar: NavbarComponent,
+      },
+    },
+    {
+      path: "/tasks",
+      name: "tasks",
+      components: {
+        default: Tasks,
+        Navbar: NavbarComponent,
+      },
+    },
+    {
+      path: "/notes",
+      name: "notes",
+      components: {
+        default: NotesView,
+        Navbar: NavbarComponent,
+      },
+      children: [
+        { path: "new", name: "newnote", component: NotesAddEdit },
+        {
+          name: "editnote",
+          path:
+            "edit/:noteId([0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12})",
+          component: NotesAddEdit,
+          props: (route) => ({ id: route.params.id }),
+        },
+      ],
+    },
+    { path: "/:pathMatch(.*)*", name: "NotFound", component: NotFound },
   ],
-  linkActiveClass: 'active',
-  linkExactActiveClass: 'active'
+  linkActiveClass: "active",
+  linkExactActiveClass: "active",
 });
 
 export default router;
--- src/store.js
+++ src/store.js
@@ -14,6 +14,7 @@ function uuidv4() {
 export const store = createStore({
   plugins: [vuexPersist.plugin],
   state: {
+    startScreen:'',
     tasks: [
      
     ],
@@ -26,6 +27,9 @@ export const store = createStore({
 
   },
   getters: {
+    startScreen(state){
+    return state.startScreen
+    },
     allNotes(state) {
       return state.notes
     },
@@ -35,7 +39,7 @@ export const store = createStore({
     allTasks(state) {
       return state.tasks
     },
-    unfinishedNoteCount(state){
+    uninishedNoteCount(state){
       return state.notes.filter(note => !note.done).length
     },
     unfinishedTaskCount(state) {
@@ -43,6 +47,9 @@ export const store = createStore({
     }
   },
   mutations: {
+    startScreen(state,value){
+      state.startScreen=value;
+   },
     saveLabel(state,label){
        if (label.id) {
         let existingLabelIndex = state.labels.findIndex(z => z.id == label.id);
